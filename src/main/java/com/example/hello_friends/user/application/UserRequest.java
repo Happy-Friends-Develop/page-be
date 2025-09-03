dbabd59815718d535d5de83d15472ee41946a3cb
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-09-03 14:10:35 +0900

    회원가입 기능 추가

--- src/main/java/com/example/hello_friends/user/application/UserRequest.java
+++ src/main/java/com/example/hello_friends/user/application/UserRequest.java
@@ -1,20 +1,36 @@
 package com.example.hello_friends.user.application;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UserRequest {
+    @NotBlank
+    @Schema(description = "아이디", example = "dev")
     private String id;
 
+    @NotBlank
+    @Schema(description = "비밀번호", example = "1111")
     private String password;
 
+    @NotBlank
+    @Schema(description = "이름", example = "개발용 계정")
     private String name;
 
+    @NotBlank
+    @Schema(description = "닉네임", example = "dev")
     private String nickname;
 
+    @NotBlank
+    @Schema(description = "전화번호", example = "010-5380-0048")
     private String phone;
 
+    @NotBlank
+    @Schema(description = "이메일", example = "0414minyoung@naver.com")
     private String email;
 
+    @NotBlank
+    @Schema(description = "주소", example = "인천 남동구 백범로 124번길 43")
     private String address;
 }
--- src/main/java/com/example/hello_friends/user/presentation/UserController.java
+++ src/main/java/com/example/hello_friends/user/presentation/UserController.java
@@ -1,15 +1,28 @@
 package com.example.hello_friends.user.presentation;
 
+import com.example.hello_friends.user.application.UserRequest;
 import com.example.hello_friends.user.application.UserService;
+import com.example.hello_friends.user.domain.User;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "사용자")
+@RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
 
+    @Operation(summary = "회원가입", description = "회원가입합니다.")
+    @PostMapping("register")
+    public User register(@RequestBody @Valid UserRequest userRequest){
+        return userService.register(userRequest);
+    }
 
 }
