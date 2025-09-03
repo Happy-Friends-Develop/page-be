840ac14dc267c6b7e8fce47647c99a2e2c8bff09
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-09-03 13:30:14 +0900

    빈 탐색기능

--- src/main/java/com/example/hello_friends/HelloFriendsApplication.java
+++ src/main/java/com/example/hello_friends/HelloFriendsApplication.java
@@ -2,8 +2,10 @@ package com.example.hello_friends;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class HelloFriendsApplication {
 
 	public static void main(String[] args) {
