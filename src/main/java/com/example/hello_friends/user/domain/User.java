5614a38bd5b986ff47ba23f4ad562d054bc61427
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-09-03 10:40:32 +0900

    유저 엔티티 생성

---
+++ src/main/java/com/example/hello_friends/user/domain/User.java
@@ -0,0 +1,14 @@
+package com.example.hello_friends.user.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "user")
+public class User {
+}
