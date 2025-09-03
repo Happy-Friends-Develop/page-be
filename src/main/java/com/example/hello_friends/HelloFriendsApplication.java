d61ff74b4cb0a54bf2eddf38cfe485d27d9e2d88
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-09-03 10:33:09 +0900

    first commit

---
+++ src/main/java/com/example/hello_friends/HelloFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hello_friends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloFriendsApplication.class, args);
+	}
+
+}
