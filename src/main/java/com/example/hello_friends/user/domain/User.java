3b8a19b5a3d6f8eaccdbcea020a432d7bbe22e99
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-09-03 13:04:23 +0900

    사용자 엔티티 칼럼 추가

--- src/main/java/com/example/hello_friends/user/domain/User.java
+++ src/main/java/com/example/hello_friends/user/domain/User.java
@@ -1,7 +1,7 @@
 package com.example.hello_friends.user.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.example.hello_friends.common.entity.EntityState;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,4 +11,34 @@ import lombok.NoArgsConstructor;
 @Getter
 @Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Long id;
+
+    @Column(name ="user_name",nullable = false)
+    private String name;
+
+    @Column(name="user_nickname", nullable = false)
+    private String nickname;
+
+    @Column(name="user_phone", nullable = false)
+    private String phone;
+
+    @Column(name="user_email", nullable = false)
+    private String email;
+
+    @Column(name="user_address", nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    @Column(name ="user_state",nullable = false)
+    @Enumerated(EnumType.STRING)
+    private EntityState state;
+
+    @Column(nullable = false)
+    private Long authId;
 }
