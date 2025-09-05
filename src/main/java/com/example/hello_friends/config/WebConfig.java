04bba0c4340ac7a197e153c60ec2f4f448345027
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-09-05 13:26:27 +0900

    multipartResolver 설정

--- src/main/java/com/example/hello_friends/config/WebConfig.java
+++ src/main/java/com/example/hello_friends/config/WebConfig.java
@@ -2,7 +2,10 @@ package com.example.hello_friends.config;
 
 import com.example.hello_friends.security.annotation.AuthArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +19,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authArgumentResolver);
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
     }
 }
