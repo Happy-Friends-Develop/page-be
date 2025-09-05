7dbd2c40840e89d66a00f608697a62ab5fe1737b
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-09-05 09:30:52 +0900

    repo 생성

--- src/main/java/com/example/hello_friends/board/domain/BoardFileRepository.java
+++ src/main/java/com/example/hello_friends/board/domain/BoardFileRepository.java
@@ -1,6 +1,8 @@
 package com.example.hello_friends.board.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BoardFileRepository extends JpaRepository<BoardFile, Long> {
 }
