14ed5672e97cc0be67cdc61d56489294f0691109
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-09-05 17:01:22 +0900

    스웨거 스키마 추가

--- src/main/java/com/example/hello_friends/security/filter/JwtPrincipalDto.java
+++ src/main/java/com/example/hello_friends/security/filter/JwtPrincipalDto.java
@@ -1,10 +1,17 @@
 package com.example.hello_friends.security.filter;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class JwtPrincipalDto {
+    @NotBlank
+    @Schema(description = "아이디", example = "5")
     private Long id; // id
+
+    @NotBlank
+    @Schema(description = "타입", example = "USER")
     private String type; // USER
 
     public JwtPrincipalDto(Long id, String type) {
