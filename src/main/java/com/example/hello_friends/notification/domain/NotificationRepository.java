ed2160ea3845ecd4fca14585b040819611380717
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-09-08 11:24:24 +0900

    repo 어노테이션 추가

--- src/main/java/com/example/hello_friends/notification/domain/NotificationRepository.java
+++ src/main/java/com/example/hello_friends/notification/domain/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.example.hello_friends.notification.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 }
--- src/main/java/com/example/hello_friends/user/domain/UserRepository.java
+++ src/main/java/com/example/hello_friends/user/domain/UserRepository.java
@@ -2,13 +2,15 @@ package com.example.hello_friends.user.domain;
 
 import com.example.hello_friends.common.entity.EntityState;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByAuthId(Long authId);
     Optional<User> findByIdAndState(Long id, EntityState state);
     List<User> findAllByState(EntityState state);
-    List<User> findAllByRole(UserRole role);
+    List<User> findAllByUserRole(UserRole userRole);
 }
