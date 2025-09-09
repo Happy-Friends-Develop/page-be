5397f3225594a47ee24112992d54f84cb75078f4
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-09-09 11:29:08 +0900

    예약 관련 엔티티 schedule에 매핑

---
+++ src/main/java/com/example/hello_friends/schedule/domain/Reservation.java
@@ -0,0 +1,33 @@
+package com.example.hello_friends.schedule.domain;
+
+import com.example.hello_friends.common.entity.LogEntity;
+import com.example.hello_friends.user.domain.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Reservation extends LogEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user; // 예약한 사용자
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule; // 예약한 스케줄 (예: 1월 1일 옵션)
+
+    @Column(nullable = false)
+    private int quantity; // 예약한 인원 수
+
+    public Reservation(User user, Schedule schedule, int quantity){
+        this.user = user;
+        this.schedule = schedule;
+        this.quantity = quantity;
+    }
+}
