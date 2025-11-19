b293ad57a339aebbc2df7e2fb7c0f9e68e1c317a
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-11-19 10:25:39 +0900

    조회 및 여부 기능 추가

--- src/main/java/com/example/hello_friends/schedule/domain/ReservationRepository.java
+++ src/main/java/com/example/hello_friends/schedule/domain/ReservationRepository.java
@@ -8,7 +8,11 @@ import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    // 예약 리스트 전부 다
     List<Reservation> findAllByUserId(Long userId);
-    boolean existsByUserAndSchedule(User user, Schedule schedule);
+    // 상태에 맞는 예약 리스트
+    List<Reservation> findAllByUserIdAndStatus(Long userId, ReservationStatus status);
+    // 상태 확인 후 예약 가능 여부
+    boolean existsByUserAndScheduleAndStatus(User user, Schedule schedule, ReservationStatus status);
 
 }
