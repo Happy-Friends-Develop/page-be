6f4b2cd20d9c221d33ad5bb2b242551f971e17d0
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-11-20 16:22:01 +0900

    비디오 및 이미지 경로 나타내기

---
+++ src/main/java/com/example/hello_friends/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.example.hello_friends.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Value("${file.upload.path}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+
+                .addResourceLocations("file:///" + uploadPath + "/image/");
+
+        registry.addResourceHandler("/videos/**")
+                .addResourceLocations("file:///" + uploadPath + "/video/");
+    }
+}
