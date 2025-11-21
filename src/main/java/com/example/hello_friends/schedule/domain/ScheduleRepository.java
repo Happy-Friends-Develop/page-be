7c8c0bb6b862e2985f297728ca5646c8d92dd14e
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-11-21 11:15:45 +0900

    동시 예약 방지 로직 추가

--- src/main/java/com/example/hello_friends/schedule/domain/ScheduleRepository.java
+++ src/main/java/com/example/hello_friends/schedule/domain/ScheduleRepository.java
@@ -1,12 +1,26 @@
 package com.example.hello_friends.schedule.domain;
 
-import com.example.hello_friends.board.domain.Board;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     List<Schedule> findAllByBoardId(Long boardId);
+
+    // 비관적 락 추가
+    // 동시에 누르는 경우 방지
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    // 3초 동안만 기다림
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "3000")})
+    @Query("select s from Schedule s where s.id = :id")
+    Optional<Schedule> findByIdWithLock(@Param("id") Long id);
 }
