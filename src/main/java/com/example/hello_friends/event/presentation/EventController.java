f6021f5b7e3646997e9791bfdd8ac435f48df259
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-11-21 16:19:22 +0900

    Redis를 통해 대기열 생성

--- src/main/java/com/example/hello_friends/event/presentation/EventController.java
+++ src/main/java/com/example/hello_friends/event/presentation/EventController.java
@@ -1,16 +1,19 @@
 package com.example.hello_friends.event.presentation;
 
+import com.example.hello_friends.common.response.MotherException;
 import com.example.hello_friends.common.response.Resp;
 import com.example.hello_friends.event.application.EventType;
 import com.example.hello_friends.event.application.request.EventCreateRequest;
 import com.example.hello_friends.event.application.response.EventResponse;
 import com.example.hello_friends.event.application.service.EventService;
+import com.example.hello_friends.redis.application.QueueService;
 import com.example.hello_friends.security.annotation.Auth;
 import com.example.hello_friends.security.filter.JwtPrincipalDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +24,7 @@ import java.util.List;
 @Tag(name = "이벤트 & 공지사항")
 public class EventController {
     private final EventService eventService;
+    private final QueueService queueService;
 
     @Operation(summary = "이벤트 생성", description = "관리자가 새로운 이벤트를 생성합니다.")
     @PostMapping("/api/admin/events")
@@ -68,8 +72,25 @@ public class EventController {
     public ResponseEntity<Resp<String>> joinEvent(
             @PathVariable Long eventId,
             @Parameter(hidden = true) @Auth JwtPrincipalDto jwtPrincipalDto) {
-        eventService.joinEvent(eventId, jwtPrincipalDto.getId());
-        return ResponseEntity.ok(Resp.ok("이벤트 참가 성공"));
+        String userIdStr = String.valueOf(jwtPrincipalDto.getId());
+
+        // 입장 권환 확인 (DB 연결 전)
+        if(!queueService.isAllowed(userIdStr)){
+            // 내 순서가 아니라면 대기열에 등록하고 돌려보냄
+            queueService.registerQueue(userIdStr);
+            Long rank = queueService.getRank(userIdStr);
+            throw new MotherException("대기 중입니다. 순번 : " + (rank + 1), HttpStatus.TOO_MANY_REQUESTS);
+        }
+
+        // 권한이 있는 50명만 여기서부터 실행
+        try {
+            eventService.joinEvent(eventId, jwtPrincipalDto.getId());
+            return ResponseEntity.ok(Resp.ok("이벤트 참가 성공"));
+        } catch (Exception e) {
+            throw new MotherException("이벤트 참가 중 오류가 발생했습니다.", HttpStatus.BAD_REQUEST);
+        } finally {
+            queueService.removeUser(userIdStr);
+        }
     }
 
     @Operation(summary = "이벤트 참가 취소", description = "사용자가 이벤트 참가를 취소합니다.")
